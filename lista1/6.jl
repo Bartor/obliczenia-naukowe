a342300ce726b5e88e48e6f6963eb2c64946edd7
Bartosz Barnaba Rajczyk <bartorkrajczyk@gmail.com>
2019-10-15 14:18:54 +0200

    poprawki, aktualizacja sprawozdania

--- lista1/6.jl
+++ lista1/6.jl
@@ -9,4 +9,5 @@ function g(number)
 end
 
 # Obliczanie obu funkcji od 8^(-1) do 8^(-180) - granica wyznaczona eksperymentalnie
-foreach(x -> println(f(8.0^(-x)), " ", g(8.0^(-x))), 1:180)
+foreach(x -> println(x, " & ", f(8.0^(-x)), " & ", g(8.0^(-x)), "\\\\\n\\hline"), 1:9)
+foreach(x -> println(20*x, " & ", f(8.0^(-20*x)), " & ", g(8.0^(-20*x)), "\\\\\n\\hline"), 1:9)
